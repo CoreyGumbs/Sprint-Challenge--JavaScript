9a87602bcf08712cc5d5832fe2a0d819144063f9
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2019-10-18 13:36:43 -0400

    [Add] Completed Arrays of objects-arry

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -104,6 +104,13 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+
+for(i=0; i < universities.length; i++){
+  if(universities[i].includes('Uni')){
+    unisWithUni.push(universities[i]);
+  };
+}
+
 console.log(unisWithUni);
 
 
